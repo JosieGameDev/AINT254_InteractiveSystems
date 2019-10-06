1211a1dedb48c257b90001e3aafa292cffad2b7d
Josie Wood <josie.wood@students.plymouth.ac.uk>
2019-10-06 21:25:24 +0100

    Started Basic Mechanic Sprint, practiced Physics
    Started my unity project for codename: tortoise, and created a basic movement script which makes the tortoise slowly walk to a given position.
    Also practiced with Unity3d physics by creating a fps, hinged door, and wrecking ball

---
+++ 254-codename🐢/Assets/Scripts/Tortoise.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tortoise : MonoBehaviour
+{
+
+    // VARIABLES
+    public Transform lettuceTransform;
+    private GameObject tortoise;
+    public float speed = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tortoise = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tortoise.transform.position = Vector3.MoveTowards(tortoise.transform.position, lettuceTransform.position, speed);
+    }
+}
