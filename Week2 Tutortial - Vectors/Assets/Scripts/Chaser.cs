d6f050e05199391ffb21c1a84667efe74ef22f06
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2019-10-10 12:59:13 +0100

    Tutorial Vector Lesson
    Played around with vectors, moving with physics etc

---
+++ Week2 Tutortial - Vectors/Assets/Scripts/Chaser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+
+    //Vars
+    #region
+    private GameObject ChaserGO;
+    private GameObject TargetGO;
+    public float speed = 5f;
+    private Vector3 ChaserVector = new Vector3(0,0,0);
+    private Vector3 TargetVector = new Vector3(0, 0, 0);
+    private Vector3 VectorDirection;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChaserGO = this.gameObject;
+        TargetGO = GameObject.FindGameObjectWithTag("Target");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChaserVector = new Vector3(ChaserGO.transform.position.x, ChaserGO.transform.position.y, ChaserGO.transform.position.z);
+        TargetVector = new Vector3(TargetGO.transform.position.x, TargetGO.transform.position.y, TargetGO.transform.position.z);
+
+        Vector3 resultVector =  Vector3.Cross(ChaserVector, TargetVector);
+        Debug.Log("result vector is " + resultVector);
+
+        //normalise vector
+        VectorDirection = resultVector / resultVector.magnitude;
+
+        Debug.Log("direction is " + VectorDirection);
+    }
+}
---
+++ Week2 Tutortial - Vectors/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    // VARS
+    #region
+    private GameObject Player;
+    public float speed = 5f;
+    private float x, z;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        Player.transform.Translate(x, 0, z * Time.deltaTime);
+    }
+
+   
+}
