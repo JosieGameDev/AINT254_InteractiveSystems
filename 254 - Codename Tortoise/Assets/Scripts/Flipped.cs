db255b7c4918d3e8f7e07c5409acd4c0bdc094af
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2019-10-24 15:54:43 +0100

    Flip ani and  spawner

--- 254 - Codename Tortoise/Assets/Scripts/Flipped.cs
+++ 254 - Codename Tortoise/Assets/Scripts/Flipped.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flipped : StateMachineBehaviour
+public class FlippedBehaviour : StateMachineBehaviour
 {
 
     //vars
---
+++ 254 - Codename Tortoise/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    // vars
+    public GameObject tortoisePrefab;
+    public float spawnRate = 5f;
+    private float lastSpawnTime;
+    public int maxTortoiseInScene = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSpawnTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (checkTortoiseInScene() < maxTortoiseInScene)
+        {
+            if (lastSpawnTime + spawnRate <= Time.time)
+            {
+                lastSpawnTime = Time.time;
+                Spawn();
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        Vector3 spawnPos = this.transform.position;
+        Instantiate(tortoisePrefab,spawnPos, Quaternion.identity );
+    }
+
+    public int checkTortoiseInScene()
+    {
+        GameObject[] tortoises = GameObject.FindGameObjectsWithTag("tortoise");
+        return tortoises.Length;
+    }
+}
