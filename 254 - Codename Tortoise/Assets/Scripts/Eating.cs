502b95e052006af7ecb2ccd00e7a45fda0cfbe2a
Josie Wood <josie.wood@students.plymouth.ac.uk>
2019-11-25 16:36:40 +0000

    adjustments from testing

--- 254 - Codename Tortoise/Assets/Scripts/Eating.cs
+++ 254 - Codename Tortoise/Assets/Scripts/Eating.cs
@@ -8,7 +8,7 @@ public class Eating : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        //get the collider go
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
